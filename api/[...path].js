0b09831efae9609369d4f783ad72fc8566923ef6
Vaishnavi-Gavi <gavivaishnavi1@gmail.com>
2025-12-09 20:42:47 +0530

    Route /api/* to api/[...path].js and export handler forwarding to Express app

--- api/[...path].js
+++ api/[...path].js
@@ -3,5 +3,8 @@ const path = require('path');
 // Import the Express app from backend
 const app = require(path.join(__dirname, '..', 'backend', 'index.js'));
 
-// Export the app as the serverless function handler
-module.exports = app;
+// Export a function handler that forwards requests to the Express app
+module.exports = (req, res) => {
+	// Express app is a function (req, res) => {}
+	return app(req, res);
+};
